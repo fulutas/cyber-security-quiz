cc34c5f0ab8a8fdac19cc0ca9471cf93906f69c2
Furkan Ulutaş <ulutasfurkan@gmail.com>
2024-05-24 22:28:49 +0300

    data question change two

--- src/data/questions.js
+++ src/data/questions.js
@@ -5,31 +5,31 @@ export const questions = [
     question: "Fidye yazılımının bir teknoloji şirketinin itibarı üzerindeki olası etkileri nelerdir?",
     options: [
       {
-        label: "A",
-        answer: "Müşteriye karşı itibar kaybı.",
+        label: "a",
+        answer: "Müşteriye karşı itibar kaybolur.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "B",
+        label: "b",
         answer: "Rekabet avantajı kaybolur.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "C",
+        label: "c",
         answer: "Hisse senedinin değeri düşer.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "D",
-        answer: "İşe alım sürecindeki olumsuzluklar.",
+        label: "d",
+        answer: "Maddi kayıplar yaşanır.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "E",
+        label: "e",
         answer: "Bu seçeneklerin tümü doğrudur.",
         isCorrect: true,
         points: 10
@@ -42,63 +42,69 @@ export const questions = [
     question: "KodKilit fidye yazılımıyla baş etmenin en etkili yöntemi nedir?",
     options: [
       {
-        label: "A",
-        answer: "Verilerin yayınlanması ve verilere yeniden erişim sağlanması için ödeme talep etmek.",
+        label: "a",
+        answer: "Verilerin yayınlanmaması ve verilere yeniden erişim sağlamak için talep edilen ödemenin yapılması.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "B",
-        answer: "Güvenlik profesyonellerinin yardımıyla hesapları hacklemeye çalışmak.",
+        label: "b",
+        answer: "Güvenlik profesyonellerinin yardımıyla kaybedilen verileri geri almaya çalışılması.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "C",
-        answer: "Fidye taleplerini reddetmek ve veri kaybını kabul etmek.",
+        label: "c",
+        answer: "Fidye taleplerini reddetmek ve veri kaybını kabul edilmesi.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "D",
-        answer: "Fidyeyi ödemeden önce yedeklenen verileri almaya çalışmak.",
+        label: "d",
+        answer: "Fidye saldırısına maruz kalınca veri kaybı yaşamamak için düzenli ve kısa aralıklı yedekleme yapılmalı.",
         isCorrect: true,
         points: 10
+      },
+      {
+        label: "e",
+        answer: "Hepsi doğru.",
+        isCorrect: false,
+        points: 0
       },
     ],
   },
   {
     questionId: 3,
     scenario: "",
-    question: "Fidye yazılımları nedeniyle mali zarara uğrayan teknoloji şirketlerinin olası etkileri nelerdir?",
+    question: "Fidye yazılımlarına maruz kalan teknoloji şirketlerinin yaşayabileceği olası kayıplar nelerdir?",
     options: [
       {
-        label: "A",
+        label: "a",
         answer: "Kurtarma maliyeti.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "B",
-        answer: "Veri kurtarma hizmetlerinin maliyeti.",
+        label: "b",
+        answer: "Kritik iş süreçlerindeki kesinti.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "C",
-        answer: "Ticari geliri korumak için fidyenin nakit karşılığını ödeyin.",
+        label: "c",
+        answer: "Ödenen fidyenin maliyeti.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "D",
-        answer: "Müşteri katılımı.",
+        label: "d",
+        answer: "İtibar kaybı.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "E",
-        answer: "Hepsi doğru",
+        label: "e",
+        answer: "Hepsi doğru.",
         isCorrect: true,
         points: 10
       },
@@ -107,35 +113,35 @@ export const questions = [
   {
     questionId: 4,
     scenario: "",
-    question: "Fidye yazılımının etkilerini azaltmak için hangi yöntemler kullanılabilir?",
+    question: "Fidye yazılımının etkilerini azaltmak için hangi yöntemler kullanılabilinir?",
     options: [
       {
-        label: "A",
-        answer: "Güvenlik yazılımının düzenli olarak güncellenmesi ve felaket politikasının sıkı bir şekilde uygulanması.",
+        label: "a",
+        answer: "Güvenlik yazılımının düzenli olarak güncellenmesi ve felaket politikasının sıkı bir şekilde uygulanmasını sağlamak.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "B",
+        label: "b",
         answer: "Şüpheli e-postaları açmamak ve bilinmeyen kaynaklardan hiçbir şey indirmemek.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "C",
+        label: "c",
         answer: "Çalışanlara sürekli güvenlik eğitimi vermek.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "D",
-        answer: "Trafik düzenlerini sürekli incelemek ve anormallikleri tanımak.",
+        label: "d",
+        answer: "Ağ trafiğini sürekli incelemek ve anormallikleri tanımlamak.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "E",
-        answer: "Hepsi Doğru",
+        label: "e",
+        answer: "Hepsi Doğru.",
         isCorrect: true,
         points: 10
       },
@@ -144,34 +150,34 @@ export const questions = [
   {
     questionId: 5,
     scenario: "",
-    question: "Fidye yazılımı genellikle nasıl yayılır?",
+    question: "Fidye yazılımı en çok hangisi ile yayılır?",
     options: [
       {
-        label: "A",
+        label: "a",
         answer: "Bir USB cihazından.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "B",
+        label: "b",
         answer: "E-posta yazışmalarından.",
         isCorrect: true,
         points: 10
       },
       {
-        label: "C",
+        label: "c",
         answer: "Facebook, Twitter ve Instagram gibi sosyal medya hesaplarından.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "D",
+        label: "d",
         answer: "Ofisin fotokopi makinesinden.",
         isCorrect: false,
         points: 0
       },
       {
-        label: "E",
+        label: "e",
         answer: "Ofisin tarayıcısından.",
         isCorrect: false,
         points: 0
@@ -181,7 +187,7 @@ export const questions = [
   {
     questionId: 6,
     scenario: "",
-    question: "Bir çalışana e-posta yoluyla 'Acil olarak şifrenizi güncellemeniz gerekmektedir, lütfen aşağıdaki bağlantıyı kullanın' şeklinde bir istek iletildiğinde izlenecek en doğru prosedür aşağıdakilerden hangisidir?",
+    question: "Bir çalışana e-posta yoluyla “Acil olarak şifrenizi güncellemeniz gerekmektedir, lütfen aşağıdaki bağlantıyı kullanın” şeklinde bir istek iletildiğinde çalışanın izleyeceği en doğru prosedür aşağıdakilerden hangisidir?",
     options: [
       {
         label: "A",
@@ -209,7 +215,7 @@ export const questions = [
       },
       {
         label: "E",
-        answer: "E-postadaki bağlantının gerçekten şirketin resmi web sitesine yönlendirdiğinden emin olmak için tıklanmalıdır.",
+        answer: "E-postadaki bağlantıya doğruluğunu teyit etmek için tıklanmalıdır.",
         isCorrect: false,
         points: 0
       },
@@ -218,11 +224,11 @@ export const questions = [
   {
     questionId: 7,
     scenario: "",
-    question: "Bir çalışan, sosyal medya üzerinden 'Tebrikler! Bir ödül kazandınız! Kişisel bilgilerinizi girerek ödülünüzü hemen alın!' şeklinde bir taleple karşılaştığında sergilemesi gereken ilk davranış aşağıdakilerden hangisidir?",
+    question: "Bir çalışan, sosyal medya üzerinden 'Tebrikler! Bir ödül kazandınız! Kişisel bilgilerinizi girerek ödülünüzü hemen alın!' şeklinde bir bildirimle karşılaştığında sergilemesi gereken ilk davranış aşağıdakilerden hangisidir?",
     options: [
       {
         label: "A",
-        answer: "Bilinmeyen kişiler tarafından gönderilen mesajları dikkate almamak veya Bilgi Teknolojileri departmanına bildirmek.",
+        answer: "Bilinmeyen kişiler tarafından gönderilen mesajları dikkate almamak.",
         isCorrect: false,
         points: 0
       },
@@ -234,13 +240,13 @@ export const questions = [
       },
       {
         label: "C",
-        answer: "İnternet üzerinden sunulan ödüllere şüphe ile yaklaşmak ve bu tür tekliflerin doğruluğunu sorgulamak.",
+        answer: "İnternet üzerinden sunulan ödüllere şüphe ile yaklaşmak ve bu durumu BT birimine bildirmek.",
         isCorrect: true,
         points: 10
       },
       {
         label: "D",
-        answer: "Şüpheli mesajları, şirketin belirlediği prosedürlere uygun olarak Bilgi Teknolojileri departmanına iletmek.",
+        answer: "Şüpheli mesajları, şirketin belirlediği prosedürlere uygun olarak incelemek.",
         isCorrect: false,
         points: 0
       },
@@ -255,29 +261,35 @@ export const questions = [
   {
     questionId: 8,
     scenario: "",
-    question: "Bir çalışanın, 'Bankanızda bir sorun mevcut, lütfen hesap bilgilerinizi gözden geçirin' şeklinde bir taleple karşılaştığı telefon araması durumunda uygulaması gereken en uygun prosedür aşağıdakilerden hangisidir?",
+    question: "Bir çalışanın, 'Bankanızda bir sorun mevcut, lütfen hesap bilgilerinizi gözden geçirin' şeklinde bir talep içeren telefon aramasına maruz kaldığında vermesi gereken en doğru tepki aşağıdakilerden hangisidir?",
     options: [
       {
         label: "A",
-        answer: "Telefon yoluyla iletilen bu tür taleplere karşılık olarak kişisel veya finansal bilgiler asla ifşa edilmemelidir.",
+        answer: "Telefon yoluyla iletilen bu tür taleplere karşın kişisel veya finansal bilgiler asla paylaşılmamalıdır.",
         isCorrect: true,
         points: 10
       },
       {
         label: "B",
-        answer: "Bankanın resmi müşteri hizmetleri ile irtibata geçerek durumun detaylı bir incelemesini talep etmek.",
+        answer: "Bankanın resmi müşteri hizmetleri ile irtibata geçilerek duruma ilişkin detaylı bir inceleme talep edilmelidir.",
         isCorrect: false,
         points: 0
       },
       {
         label: "C",
-        answer: "Tanınmayan numaralardan gelen aramalara yanıt vermemek ve otomatik olarak reddetmek.",
+        answer: "Tanınmayan numaralardan gelen aramalar yanıtlanmamalı ve otomatik olarak reddedilmelidir.",
         isCorrect: false,
         points: 0
       },
       {
         label: "D",
-        answer: "Şüpheli telefon görüşmelerini, bilgi işlem birimine rapor ederek şirketin güvenlik politikalarına uygun hareket etmek.",
+        answer: "Şüpheli telefon görüşmeleri, bilgi işlem birimine rapor edilmelidir.",
+        isCorrect: false,
+        points: 0
+      },
+      {
+        label: "E",
+        answer: "Şirketin güvenlik politikalarına uygun hareket edilmelidir.",
         isCorrect: false,
         points: 0
       },
@@ -296,7 +308,7 @@ export const questions = [
       },
       {
         label: "B",
-        answer: "Ofis ekipmanlarının izinsiz kullanımını önlemek için gerekli güvenlik tedbirleri alınmalıdır.",
+        answer: "Ofis ekipmanları başkasına kullandırılırken gerekli güvenlik tedbirleri alınmalıdır.",
         isCorrect: false,
         points: 0
       },
@@ -308,13 +320,13 @@ export const questions = [
       },
       {
         label: "D",
-        answer: "Bilgisayar başından ayrıldığınızda, cihazın kilitlemesi veya güvenlik duvarı ile korunması gerekmektedir.",
+        answer: "Bilgisayar başından ayrıldığınızda, cihaz kilitlenmeli ve güvenlik duvarı ile korunmalıdır.",
         isCorrect: false,
         points: 0
       },
       {
         label: "E",
-        answer: "Ofis güvenlik politikalarının periyodik olarak gözden geçirilmesi ve çalışanlara yönelik güvenlik bilinci eğitimlerinin düzenlenmesi önemlidir.",
+        answer: "Ofis güvenlik politikaları periyodik olarak gözden geçirilmeli ve çalışanlara yönelik farkındalık eğitimleri düzenlenmelidir.",
         isCorrect: false,
         points: 0
       },
@@ -323,23 +335,23 @@ export const questions = [
   {
     questionId: 10,
     scenario: "",
-    question: "Bir iş yerindeki kutlama gibi büyük organizasyonlar sırasında, Wi-Fi ağının güvenliğini sağlamak amacıyla alınması gereken tedbir aşağıdakilerden hangisidir?",
+    question: "Bir iş yerindeki kutlama gibi büyük organizasyon sırasında, kurum dışı misafirlerin Wi-Fi ağına bağlanma ihtiyacını gidermek amacıyla alınması gereken tedbir aşağıdakilerden hangisidir?",
     options: [
       {
         label: "A",
-        answer: "Kişisel bilgiler, Wi-Fi ağına bağlandığınızda, özellikle büyük etkinliklerde, güvenlik önlemleri göz ardı edilmeden paylaşılmamalıdır.",
+        answer: "Wi-Fi ağına bağlanıldığında, güvenlik önlemleri alınmadan kişisel veriler paylaşılmamalıdır.",
         isCorrect: false,
         points: 0
       },
       {
         label: "B",
-        answer: "Büyük etkinlikler öncesinde, Wi-Fi ağına bağlanmak için Bilgi Teknolojileri (BT) sistemi tarafından onaylanmış ve yetkilendirilmiş ağların tercih edilmesi gerekmektedir.",
+        answer: "Kurum dışı misafirler için ayrı bir misafir ağı tesis edilmeli ve kurum sistemlerinden izole edilmelidir.",
         isCorrect: true,
         points: 10
       },
       {
         label: "C",
-        answer: "Kamusal alanlarda Wi-Fi kullanımında, SSL şifrelemesi gibi güvenlik protokollerini destekleyen ağların kullanılması önem taşımaktadır.",
+        answer: "Kamusal alanlarda Wi-Fi kullanımında, SSL şifrelemesi gibi güvenlik protokollerini destekleyen ağların kullanılması önemlidir.",
         isCorrect: false,
         points: 0
       },
@@ -351,7 +363,7 @@ export const questions = [
       },
       {
         label: "E",
-        answer: "Büyük etkinliklerde Wi-Fi ağına bağlanırken, güvenlik duvarları ve antivirüs yazılımları gibi güvenlik önlemlerinin aktif hale getirilmesi şarttır.",
+        answer: "Büyük etkinliklerde Wi-Fi ağına bağlanırken, internet hızının düşmesini engellemek için güvenlik duvarları ve antivirüs yazılımları gibi güvenlik önlemleri durdurulabilir.",
         isCorrect: false,
         points: 0
       },
